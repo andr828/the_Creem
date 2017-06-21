f49a774224001190686bb49d4d42fc09161593f6
Andrei Kreanga <andreyfomen828@gmail.com>
2017-06-22 02:36:48 +0300

    set sign up params

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -34,6 +34,10 @@ class Auth::RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def user_attrs
+    %w(id email created_at updated_at)
+  end
+
   def sign_up_params
     allow = [:email, :password, :password_confirmation, [entity_attributes: [:first_name, :last_name, :business_name, :type]]]
     user = params.require(resource_name).permit(allow)
@@ -41,11 +45,7 @@ class Auth::RegistrationsController < Devise::RegistrationsController
     user[:entity_attributes][:type] = (user[:entity_attributes][:type] || '').camelize
     user
   end
-
-  def user_attrs
-    %w(id email created_at updated_at)
-  end
-
+  
   def entity_attrs
     %w(id type first_name last_name business_name metadata created_at updated_at)
   end
